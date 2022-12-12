46b819d235031682d94220fc4ccebc475304553f
harihnhn <hari2005999@gmail.com>
2022-12-12 12:41:16 +0530

    cmt

---
+++ C#/1_exercise_for_c#/types_of_class_s/types_of_class_p/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace types_of_class_p
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
